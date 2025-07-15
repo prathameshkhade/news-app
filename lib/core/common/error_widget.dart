7bd7ffc0032db2bb417d63e661dafd5444968c56
prathameshkhade <122447678+prathameshkhade@users.noreply.github.com>
2025-07-15 18:54:04 +0530

    feat: add error and loader widgets for improved UI feedback

---
+++ lib/core/common/error_widget.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../errors/failure.dart';
+
+class ErrorWidget extends StatelessWidget {
+  final Failure failure;
+
+  const ErrorWidget({super.key, required this.failure});
+
+  @override
+  Widget build(BuildContext context) {
+    final errColor = CupertinoColors.destructiveRed;
+    final theme = Theme.of(context);
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      spacing: 30,
+      children: <Widget>[
+        // Icon with status code
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          spacing: 25,
+          children: <Widget>[
+            Icon(CupertinoIcons.exclamationmark_bubble, color: errColor),
+            Text(
+              failure.statusCode.toString(),
+              style: theme.textTheme.titleLarge?.copyWith(color: errColor),
+            ),
+          ],
+        ),
+
+        // Text with error message
+        Text(
+          failure.message,
+          style: theme.textTheme.titleLarge?.copyWith(color: errColor),
+        ),
+      ],
+    );
+  }
+}
