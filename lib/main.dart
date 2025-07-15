f484c813e422a32457d5abdb6fba5b88c0db5afe
prathameshkhade <122447678+prathameshkhade@users.noreply.github.com>
2025-07-15 17:21:47 +0530

    feat: update app structure to support news features with dynamic theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,42 @@
-import 'package:amazon/features/home/presentation/pages/home_page.dart';
+import 'package:amazon/features/show_news/presentation/screens/home_screen.dart';
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const AmazonMaterialApp());
+  runApp(const NewsApp());
 }
 
-class AmazonMaterialApp extends StatelessWidget {
-  const AmazonMaterialApp({super.key});
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Amazon Clone',
-      theme: ThemeData.dark(
-        useMaterial3: true,
-      ),
-      home: const HomePage(),
+    return DynamicColorBuilder(
+      builder: (lightDynamic, darkDynamic) {
+        return MaterialApp(
+            title: 'News App',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              fontFamily: 'JetBrainsMonoNerd',
+              useMaterial3: true,
+              colorScheme: darkDynamic ?? ColorScheme.fromSeed(
+                seedColor: Colors.blue,
+                brightness: Brightness.light,
+              ),
+            ),
+            darkTheme: ThemeData(
+              fontFamily: 'JetBrainsMonoNerd',
+              useMaterial3: true,
+              colorScheme: darkDynamic ?? ColorScheme.fromSeed(
+                seedColor: Colors.blue,
+                brightness: Brightness.dark,
+              ),
+            ),
+            themeMode: ThemeMode.system,
+            home: const HomeScreen(),
+        );
+      },
     );
   }
 }
-
-
